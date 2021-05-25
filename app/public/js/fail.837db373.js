0b6fd6938389d2b56c3ba3b6567b97dae6d6f9cf
gaoshuaixing <gaoshuaixing@bilibili.com>
2021-05-25 17:40:48 +0800

    new web

--- app/public/js/fail.837db373.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["fail"],{"8cdb":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" 404 page ")])},c=[],i={name:"404"},l=i,r=t("2877"),s=Object(r["a"])(l,a,c,!1,null,"52088166",null);n["default"]=s.exports}}]);
